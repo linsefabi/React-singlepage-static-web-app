4f6372c83490a6a3fb01f70b7508d195a87040f4
Rajmohan Velmurugan <rajmohan@velmicrosystems.com>
2024-08-26 12:37:13 +0530

    addedname

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p> Hi Fabi, Your app is Running. Great !!!! </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
